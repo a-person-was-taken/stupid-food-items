3de8bf49ba9789602c6bc38d091b878adc829e69
A Person <164549cp@gmail.com>
2025-05-06 21:49:00 -0700

    Bug fixes

--- src/client/java/com/me/stupidfooditems/client/StupidfooditemsClient.java
+++ src/client/java/com/me/stupidfooditems/client/StupidfooditemsClient.java
@@ -3,7 +3,6 @@ package com.me.stupidfooditems.client;
 import com.me.stupidfooditems.Stupidfooditems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
@@ -11,25 +10,7 @@ import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.PlayerInput;
 
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.network.RegistryByteBuf;
-import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.network.packet.CustomPayload;
-
 public class StupidfooditemsClient implements ClientModInitializer {
-    public record PlayerInputC2SPayload(PlayerInput input) implements CustomPayload {
-        public static final Identifier INPUT_PACKET_PAYLOAD_ID = Identifier.of(Stupidfooditems.StupidFoods.MOD_ID, "input_packet");
-        public static final CustomPayload.Id<PlayerInputC2SPayload> ID = new CustomPayload.Id<>(INPUT_PACKET_PAYLOAD_ID);
-        public static final PacketCodec<RegistryByteBuf, PlayerInputC2SPayload> CODEC = PacketCodec.tuple(
-                PlayerInput.PACKET_CODEC, PlayerInputC2SPayload::input,
-                PlayerInputC2SPayload::new
-        );
-
-        @Override
-        public Id<? extends CustomPayload> getId() {
-            return ID;
-        }
-    }
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
