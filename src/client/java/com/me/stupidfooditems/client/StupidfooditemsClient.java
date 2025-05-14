b77f25932469f3d02fdb2cd6596bd48cde4fdcdc
A Person <200301366+a-person-was-taken@users.noreply.github.com>
2025-05-14 11:41:39 -0700

    Incomplete

--- src/client/java/com/me/stupidfooditems/client/StupidfooditemsClient.java
+++ src/client/java/com/me/stupidfooditems/client/StupidfooditemsClient.java
@@ -11,6 +11,7 @@ import net.minecraft.particle.ParticleTypes;
 public class StupidfooditemsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.attachLayerBefore(IdentifiedLayer.CHAT, EXAMPLE_LAYER, HudRenderingEntrypoint::render));
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null) return;
             if (!SimpleConfig.PARTICLE_EFFECTS_ENABLED) return;
@@ -28,6 +29,23 @@ public class StupidfooditemsClient implements ClientModInitializer {
         });
     }
 
+    private static void render(DrawContext context, RenderTickCounter tickCounter) {
+		int color = 0xFFFF0000; // Red
+		int targetColor = 0xFF00FF00; // Green
+
+		// You can use the Util.getMeasuringTimeMs() function to get the current time in milliseconds.
+		// Divide by 1000 to get seconds.
+		double currentTime = Util.getMeasuringTimeMs() / 1000.0;
+
+		// "lerp" simply means "linear interpolation", which is a fancy way of saying "blend".
+		float lerpedAmount = MathHelper.abs(MathHelper.sin((float) currentTime));
+		int lerpedColor = ColorHelper.lerp(lerpedAmount, color, targetColor);
+
+		// Draw a square with the lerped color.
+		// x1, x2, y1, y2, z, color
+		context.fill(0, 0, 10, 10, 0, lerpedColor);
+	}
+    
     private void spawnParticle(MinecraftClient client, ParticleEffect effect) {
         if (client.world != null) {
             assert client.player != null;
